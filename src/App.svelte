f4a54a6ee169583ee6aff97e8ea0a21d21359366
Manassarn Manoonchai <manassarn@gmail.com>
2021-09-03 21:40:31 +0700

    Porsor - Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -7,15 +7,29 @@
   import Menu from "./lib/Menu.svelte"
   import Social from "./lib/Social.svelte"
 
-  const url = "https://single-page-svelte.vercel.app"
-  const title = "Single Page Svelte"
+  const url = "https://porsor.vercel.app"
+  const title = "PorSor Name Generator"
 
-  const menuItems = [{ name: "Github", url: "https://github.com/narze/single-page-svelte" }]
+  const menuItems = [{ name: "Github", url: "https://github.com/narze/porsor" }]
 
   const description = "Build a single page app with Svelte, quickly."
   const imageUrl =
-    "https://raw.githubusercontent.com/narze/timelapse/master/projects/single-page-svelte_home.png"
+    "https://raw.githubusercontent.com/narze/timelapse/master/projects/porsor_home.png"
   const gtagId = null
+
+  let output: string = ""
+  let input = "ธนานนท์ ปฏิญญาศักดิกุล"
+
+  function generate() {
+    let [name, surname] = input.split(" ", 2)
+    name = name.replace(/์$/, "")
+    surname = surname.replace(/์$/, "")
+    const surnameChars = surname.split("")
+    const nameChars = name.split("")
+    name = [nameChars.slice(0, -1).join(""), "โ", nameChars.slice(-1).join("")].join("")
+    surname = [surnameChars.slice(0, -1).join(""), "โ", surnameChars.slice(-1).join("")].join("")
+    output = [name, surname].join(" ")
+  }
 </script>
 
 <Kofi name="narze" label="Support Me" />
@@ -23,10 +37,20 @@
 <Social {url} {title} />
 <Head {title} {description} {url} {imageUrl} {gtagId} />
 
-<main class="w-full h-screen flex flex-col justify-center items-center">
-  <h1 class="text-6xl text-green-400 flex flex-col">
-    <span>Single</span><span>Page</span><span>Svelte</span>
-  </h1>
+<main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
+  <h1 class="text-6xl text-green-400 flex flex-col">PorSor Name Generator</h1>
+  <input type="text" class="border w-60 px-2 py-1 rounded" bind:value={input} />
+
+  <button class="border w-60 px-2 py-1 rounded" on:click={generate}>Generate</button>
+
+  <input
+    type="text"
+    class="border w-60 px-2 py-1 rounded"
+    placeholder="ฉายาของคุณ"
+    value={output}
+  />
+
+  <button class="border w-60 px-2 py-1 rounded" on:click={() => {}}>Copy</button>
 </main>
 
 <style>
