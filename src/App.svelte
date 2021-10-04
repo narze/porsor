d5ab30cb4c3bf7d6799d2a58abd054374b9fced6
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-10-04 11:53:10 +0700

    ตาลปุตฺโต

--- src/App.svelte
+++ src/App.svelte
@@ -123,6 +123,9 @@
     function สิโต(str: string) {
       return str + "สิโต"
     },
+    function ตาลปุตฺโต(str: string) {
+      return str + "ตาลปุตฺโต"
+    },
   ]
 
   function copyOutput() {
